0ea716e1e20ef388f35e089951bdf338a79b7f4b
Tim Pastor <141689447+T-Pastor@users.noreply.github.com>
2024-12-22 18:53:31 -0500

    created initial functions

---
+++ script.js
@@ -0,0 +1,39 @@
+const num1 = undefined;
+const num2 = undefined;
+const operator = '';
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(num1, num2, operator) {
+    switch(operator) {
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, numw);
+            break;
+        default:
+            alert("That's not a valid operation");
+    }
+}
