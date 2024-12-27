b8015d9c1b2d7e07303d6e6abe41b0ad91c7892c
Tim Pastor <141689447+T-Pastor@users.noreply.github.com>
2024-12-26 20:20:44 -0500

    working through calculator functions, periodic commit

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-const num1 = undefined;
-const num2 = undefined;
-const operator = '';
-
+let screenText = []; 
+let displayNumber = document.getElementById("display");
 
 function add(a, b) {
     return a + b;
@@ -19,21 +17,88 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(num1, num2, operator) {
+function operate(a, b, operator) {
     switch(operator) {
-        case '+':
-            add(num1, num2);
+        case 'add':
+            add(a, b);
             break;
-        case '-':
-            subtract(num1, num2);
+        case 'subtract':
+            subtract(a, b);
             break;
-        case '*':
-            multiply(num1, num2);
+        case 'multiply':
+            multiply(a, b);
             break;
-        case '/':
-            divide(num1, numw);
+        case 'divide':
+            divide(a, b);
             break;
         default:
             alert("That's not a valid operation");
+    }
+}   
+
+const buttons = document.getElementById("buttons");
+buttons.addEventListener("click", (event) => {
+    handleClick(event.target);
+});
+    
+function updateDisplay(text) {
+    screenText.push(text);
+    
+    
+    displayNumber.innerHTML = screenText.join(''); 
+    displayNumber.style.fontSize = "120px";
+    displayNumber.style.color = "white";
+    displayNumber.style.fontWeight = "bold";
+    displayNumber.style.alignSelf = "flex-end"
+}
+    
+function handleClick(target) {   
+    let operatorPressed = false;
+    let equalPressed = false;
+    let firstNum = [];
+    let operator = '';
+    let secondNum = [];
+    let id = target.id;
+    let targetClass = target.className;
+
+    if (targetClass === 'operator') {
+        operator = id;
+    } else if (targetClass === 'number') {
+        let number = getNumber(id);
+        updateDisplay(number);
+        firstNum.push(number);
+    } else {
+        let answer = operate(parseInt(firstNum.join('')), parseInt(secondNum.join('')), operator);
+        displayNumber.innerHTML = answer;
+
+    } 
+
+}
+
+function getNumber(id) {
+    switch(id) {
+        case "zero":
+            return 0;
+        case "one":
+            return 1;
+        case "two":
+            return 2;
+        case "three":
+            return 3;
+        case "four":
+            return 4;
+        case "five":
+            return 5;
+        case "six":
+            return 6;
+        case "seven":
+            return 7;
+        case "eight":
+            return 8;
+        case "nine":
+            return 9;
+        default:
+            return 0;
+        
     }
 }
