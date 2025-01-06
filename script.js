4c64507a9aa6df361386d4f6e994397fafdc6974
Tim Pastor <141689447+T-Pastor@users.noreply.github.com>
2025-01-05 21:22:43 -0500

    completed all primary odin objectives, working on extra credit backspace button

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let input = [];
 let screenText = []; 
 let displayNumber = document.getElementById("digits");
-let input = [];
+let operatorIndex = 0;
+let operator = '';
+let operatorPressed = false;
 
 function add(a, b) {
     return a + b;
@@ -15,10 +18,18 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b === 0) {
+        alert("Ya can't do that");
+        allClear();
+        return;
+    }
     return a / b;
 }
 
-//takes in 2 numbers and an operator and performs the proper function based on the operator
+function exponent(a, b) {
+    return a ** b;
+}
+
 function operate(a, b, operator) {
     switch(operator) {
         case '+':
@@ -33,6 +44,8 @@ function operate(a, b, operator) {
         case '/':
             return divide(a, b);
             break;
+        case '**':
+            return exponent(a, b);
         default:
             alert("That's not a valid operation");
     }
@@ -45,90 +58,111 @@ buttons.addEventListener("click", (event) => {
   
 //updates "display" on screen
 function updateDisplay(text) {
-    screenText.push(text); //pushes next digit to end of the screenText array
-    displayNumber.innerHTML = screenText.join(''); //displays screenText array
+    screenText.push(text);
+    displayNumber.innerHTML = screenText.join('');
 }
     
-function handleClick(target) {   
-        let id = target.id;
-        let targetClass = target.className;
-    
-        if (id === 'equals') {
-            let character = getNumber(id);
-            input.push(character);
-            let answer = solve(input);
-            clearScreen();
-            updateDisplay(answer);
-        } else if (targetClass === 'operator') {
-            clearScreen();
-            let character = getNumber(id);
-            input.push(character);
+function handleClick(target) {
+
+    if(!operatorPressed && target.id === 'equals') {
+        alert("Error: Not a full equation");
+        input.pop();
+        clearScreen();
+    } else if (operatorPressed && target.id === 'equals') {
+        let value = getNumber(target.id);
+        input.push(value);
+        let answer = solve(input);
+        clearScreen();
+        updateDisplay(answer);
+    } else if (target.className === 'operator') {
+        if (operatorPressed) {
+            alert(`You've already chosen ${operator} for this equation`);
         } else {
-            let character = getNumber(id);
-            input.push(character);
-            updateDisplay(getNumber(target.id))
+            clearScreen();
+            operator = getOperator(target.id);
+            input.push(operator);
+            operatorPressed = true;
+            operatorIndex = findOperatorIndex(input);
         }
+    } else if (target.className === 'number'){
+        let value = getNumber(target.id);
+        input.push(value);
+        updateDisplay(getNumber(target.id))
+    } else if (target.className === 'function') {
+        updateDisplay(getFunction(target.id));
+    }
+}
+
+function getFunction(id) {
+    switch(id) {
+        case 'clear':
+            allClear();
+            break;
+        case 'delete':
+            deleteCharacter();
+            break;
+    }
+}
+
+function getOperator(id) {
+    switch(id) {
+        case 'add':
+            return '+';
+            break;
+        case 'subtract':
+            return '-';
+            break;
+        case 'multiply':
+            return '*';
+            break;
+        case 'divide':
+            return '/';
+        case 'exponent':
+            return '**';
+            break;
+        case 'equals':
+            return '=';
+            break;
+    }
 }
 
 function getNumber(id) {
     switch(id) {
-        case "zero":
+        case 'zero':
             return 0;
             break;
-        case "one":
+        case 'one':
             return 1;
             break;
-        case "two":
+        case 'two':
             return 2;
             break;
-        case "three":
+        case 'three':
             return 3;
             break;
-        case "four":
+        case 'four':
             return 4;
             break;
-        case "five":
+        case 'five':
             return 5;
             break;
-        case "six":
+        case 'six':
             return 6;
             break;
-        case "seven":
+        case 'seven':
             return 7;
             break;
-        case "eight":
+        case 'eight':
             return 8;
             break;
-        case "nine":
+        case 'nine':
             return 9;
             break;
-        case "decimal":
+        case 'decimal':
             return '.';
             break;
-        case "add":
-            return '+';
-            break;
-        case "subtract":
-            return '-';
-            break;
-        case "multiply":
-            return '*';
-            break;
-        case "divide":
-            return '/';
-        case "equals":
-            return '=';
-            break;
-        case "pi":
+        case 'pi':
             return Math.PI.toFixed(5);
-            break;
-        case "exponent":
-            return 
-        case "clear":
-            clearScreen();
-            break;
-        case "backspace":
-            deleteCharacter();
             break;
         default:
             break; 
@@ -136,32 +170,40 @@ function getNumber(id) {
 }
 
 function solve(array) {
-    let firstNum = [];
-    let secondNum = [];
-    let operator = '';
-    let answer = 0;
-    let operatorPressed = false;
+    let firstNum = parseFloat(array.slice(0, operatorIndex).join(''));
+    let secondNum = parseFloat(array.slice(operatorIndex + 1, array.length - 1).join(''));
 
-    array.forEach((item) => {
-        if (item === '=') {
-            answer = operate(parseInt(firstNum.join('')), parseInt(secondNum.join('')), operator);
-        } else if (typeof item === 'string' && ) {
-            operator = item;
-            operatorPressed = true;
-        } else if (operatorPressed && typeof item === 'number') {
-            secondNum.push(item);
-        } else if (!operatorPressed && typeof item === 'number') {
-            firstNum.push(item);
-        }
-    })
-    return answer;
+    if(!secondNum && secondNum !== 0) {
+        alert("You're missing a second number!");
+        allClear();
+        return;
+    }
+
+    return isFloat(array) ? operate(firstNum, secondNum, operator).toPrecision(8) : operate(firstNum, secondNum, operator);
 }
 
 function clearScreen() {
     screenText = [];
 }
 
+function allClear() {
+    screenText = [];
+    input = [];
+    operatorPressed = false;
+}
+
 function deleteCharacter() {
-    screenText.pop();
     input.pop();
+}
+
+function isNumber(number) {
+    return !isNaN(number);
+}
+
+function findOperatorIndex(array) {
+    return array.findIndex(value => typeof value === "string" && value !== '.');
+}
+
+function isFloat(array) {
+    return array.includes(".");
 }
